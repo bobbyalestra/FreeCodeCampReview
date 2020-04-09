d740e401508c3093e8fb2fc21faafefd5283987d
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-09 19:18:23 -0400

    started arrays

---
+++ nestedarrays.js
@@ -0,0 +1,5 @@
+
+
+let candies =["Fruity", "Chocolate", "Peanut Butter"];
+let candyTypes =[['Fruit Rollups', 'Life Savers', 'Skittles'], ['Herseys', 'Cadbury', 'Milky Way'], ['Reeses', 'Butterfinger', 'Mr. Good Bar']];
+
