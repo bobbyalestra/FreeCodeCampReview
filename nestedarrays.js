58ec9a32abfde91a3174fc1bcf55e71b7d07f5a8
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-09 19:28:15 -0400

    added a for loop

--- nestedarrays.js
+++ nestedarrays.js
@@ -3,3 +3,7 @@
 let candies =["Fruity", "Chocolate", "Peanut Butter"];
 let candyTypes =[['Fruit Rollups', 'Life Savers', 'Skittles'], ['Herseys', 'Cadbury', 'Milky Way'], ['Reeses', 'Butterfinger', 'Mr. Good Bar']];
 
+for (i = 0; i < candyTypes.length; i++) {
+    for (j = 0; j < candies[i].length; j++) {
+    }
+}
