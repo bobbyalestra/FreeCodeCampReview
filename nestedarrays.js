5bf0700deaa74462047218042d365d2301fbdc10
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-11 11:19:07 -0400

    updates

--- nestedarrays.js
+++ nestedarrays.js
@@ -3,7 +3,11 @@
 let candies =["Fruity", "Chocolate", "Peanut Butter"];
 let candyTypes =[['Fruit Rollups', 'Life Savers', 'Skittles'], ['Herseys', 'Cadbury', 'Milky Way'], ['Reeses', 'Butterfinger', 'Mr. Good Bar']];
 
-for (i = 0; i < candyTypes.length; i++) {
-    for (j = 0; j < candies[i].length; j++) {
+for (i = 0; i < candies.length; i++) {
+    for (j = 0; j < candyTypes[i].length; j++) {
+        console.log(`  ${candies[i]} :  ${candyTypes[i][j]}  `)
+    
     }
+
 }
+
