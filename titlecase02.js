1758577de194d6b4d7e480e5410a4ef85a3f8622
Robert Alestra <bobbyalestra135@gmail.com>
2020-05-21 22:03:14 -0400

    new update with clean up

--- titlecase02.js
+++ titlecase02.js
@@ -3,21 +3,8 @@ function titleCase(str){
     let newSentence = "";
     for(i = 0; i < array.length; i++){
         newSentence += array[i][0].toUpperCase() + array[i].substr(1) + " "; 
-    }
-    
-    let newWord = "";
-
-    for( i = 0; i < str.length; i++){
-        newWord = newWord + str[i];
-     
-
-        if(str[i] == " "){
-            newWord = newWord + str[i+1].toUpperCase();
-            i++;
-        }     
-        }
-         console.log(newWord);
+    }   
+    console.log(newSentence)
     return str;
 }
-
 titleCase("I'm a little tea pot");
